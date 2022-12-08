27c2007b147ca7058aa5b6d664f84133a6889f28
bertug.kara <bertugkara@outlook.com>
2022-12-09 00:52:27 +0300

    Query Alisverisi tamamlandi

---
+++ src/main/java/com/housepredictionbackend/Query/Controller/QueryController.java
@@ -0,0 +1,26 @@
+package com.housepredictionbackend.Query.Controller;
+
+import com.housepredictionbackend.Query.DTO.QueryRequest;
+import com.housepredictionbackend.Query.Mapper.QueryMapper;
+import com.housepredictionbackend.Query.Service.QueryService;
+import com.housepredictionbackend.utilities.DataResult;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RequiredArgsConstructor
+@CrossOrigin
+@RestController
+@RequestMapping(path = "api/query/")
+public class QueryController {
+
+    private final QueryService queryService;
+
+    @PostMapping("receiveQuery")
+    public DataResult<Integer> query(@RequestBody @Valid QueryRequest queryRequest) {
+        return new DataResult<>(queryService.performQueryOperations
+                (QueryMapper.INSTANCE.dtoToEntity(queryRequest)),
+                true, "Operation Completed");
+    }
+}
