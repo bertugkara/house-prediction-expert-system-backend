5da9c5f63ff595abe5b0fecf0aa09c914359d7e2
bertugkara <bertugkara@outlook.com>
2022-12-10 01:27:28 +0300

    Json save eklendi

--- src/main/java/com/housepredictionbackend/Rule/Controller/RuleController.java
+++ src/main/java/com/housepredictionbackend/Rule/Controller/RuleController.java
@@ -1,18 +1,14 @@
 package com.housepredictionbackend.Rule.Controller;
 
-import com.fasterxml.jackson.annotation.JsonValue;
 import com.housepredictionbackend.Rule.Service.RuleService;
 import com.housepredictionbackend.utilities.DataResult;
-import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
-import org.json.JSONObject;
 import org.json.simple.parser.ParseException;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.FileNotFoundException;
 import java.io.IOException;
 
 @RequiredArgsConstructor
@@ -20,9 +16,7 @@ import java.io.IOException;
 @RestController
 @RequestMapping(path = "api/rule")
 public class RuleController {
-
     private final RuleService ruleService;
-
     @GetMapping("/getRules")
     public DataResult<Object> returnRules() throws IOException, ParseException {
         return ruleService.returnRules();
